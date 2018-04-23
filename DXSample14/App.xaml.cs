d64cf97f4c8256b88adddfa3d85d3aec009d74f2
Florian Koch <fkoch@selectline.eu>
2018-04-23 07:36:14 +0200

    created sample project with theme

---
+++ DXSample14/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Core;
+
+namespace DXSample14
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var theme = new Theme("SelectLineCrmClassic", "SelectLine Crm Classic", "SelectLine Themes")
+            {
+                AssemblyName = "DevExpress.Xpf.Themes.SelectLineCrmClassic.v17.2"
+            };
+            Theme.RegisterTheme(theme);
+            ApplicationThemeHelper.ApplicationThemeName = theme.Name;
+        }
+    }
+}
---
+++ DXSample14/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpf.Ribbon;
+
+namespace DXSample14
+{
+    public partial class MainWindow : DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
